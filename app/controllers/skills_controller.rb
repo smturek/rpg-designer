31e98e22b54a930680dca110a7a6b451159c5d17
smturek <smturek@gmail.com>
2014-12-17 15:57:30 -0700

    finished skill crud

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -11,12 +11,29 @@ class SkillsController < ApplicationController
   def create
     @skill = Skill.new(skill_params)
     @skill.save
+    redirect_to skills_path
+  end
+
+  def destroy
+    set_skill
+    @skill.destroy
+    redirect_to skills_path
+  end
+
+  def update
+    set_skill
+    @skill.update(skill_params)
+    redirect_to skills_path
   end
 
   private
 
   def skill_params
     params.require(:skill).permit(:name, :cost)
+  end
+
+  def set_skill
+    @skill = Skill.find(params[:id])
   end
 
 end
