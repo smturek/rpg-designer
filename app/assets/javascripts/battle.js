361e1477713931cf4426cc9815c12f73a6d21299
smturek <smturek@gmail.com>
2014-12-24 23:11:33 -0700

    cleaned up battle page

--- app/assets/javascripts/battle.js
+++ app/assets/javascripts/battle.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
 
   $('button').on('click', function() {
-    $(this).hide();
+    this.$(parent()).find('.fa.fa-bug.fa-5x').hide();
   });
 
   $('.fa.fa-bug.fa-5x').on('click', function() {
-    $(this).animate({'left': '-500px'})
+    $(this).animate({'left': '-550px'})
     $(this).animate({'left': '0px'})
+  });
+
+  $('.fa.fa-male.fa-5x').on('click', function() {
+    $(this).animate({'right': '-550px'})
+    $(this).animate({'right': '0px'})
   });
 
 });
